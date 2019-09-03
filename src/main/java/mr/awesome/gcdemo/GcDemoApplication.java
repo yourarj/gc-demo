774b834422b9cf9b69e94fd25485a4078526f7e8
Arjun Shinde <arjun.shinde.ext@siemens.com>
2019-09-03 13:13:58 +0530

    sdf
    Initial commit
    
    Signed-off-by: Arjun Shinde <arjun.shinde.ext@siemens.com>
    
     On branch master
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   build.gradle
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   settings.gradle
    	new file:   src/main/java/mr/awesome/gcdemo/GcDemoApplication.java
    	new file:   src/main/resources/application.properties
    	new file:   src/test/java/mr/awesome/gcdemo/GcDemoApplicationTests.java

---
+++ src/main/java/mr/awesome/gcdemo/GcDemoApplication.java
@@ -0,0 +1,13 @@
+package mr.awesome.gcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mr/awesome/gcdemo/GcDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package mr.awesome.gcdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GcDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
